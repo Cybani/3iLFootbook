ca7dc6f9c3a34c1a04417f980807f21281a9caff
Baptiste Cubaut <baptistecubaut@orange.fr>
2026-09-14 18:25:09 +0200

    Pages de connexion/inscription sans php

---
+++ php/connexion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+
+<section>
+        <div>
+            Connectez vous
+        </div>
+        <br>
+        <br>
+        <div>
+            <label for="email">Adresse mail</label>
+            <input type="text" placeholder="Ex : 3iLStudent@3il.fr">
+        </div>
+        <br>
+        <div>
+        <label for="mdp">Mot de passe</label>
+        <input type="text" placeholder="Ex : motdepasse123">
+        </div>
+        <br>
+        <div>
+            <button type="submit">Valider</button>
+        </div>
+        <div>
+            Pas de compte ? <a href="inscription.php">Inscrivez vous</a>
+        </div>
+    </section>
+    
+</body>
+</html>
---
+++ php/inscription.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    
+</head>
+<body>
+
+    <section>
+        <div>
+            Inscrivez vous
+        </div>
+        <br>
+        <div>
+        <label for="nom">Nom</label>
+        <input type="text" placeholder="Ex : Paul">
+        </div>
+        <br>
+        <div>
+            <label for="email">Adresse mail</label>
+            <input type="text" placeholder="Ex : 3iLStudent@3il.fr">
+        </div>
+        <br>
+        <div>
+        <label for="mdp">Mot de passe</label>
+        <input type="text" placeholder="Ex : motdepasse123">
+        </div>
+        <br>
+        <div>
+            <button type="submit">Valider</button>
+        </div>
+        <div>
+            Vous avez déjà un compte ? <a href="connexion.php">Connectez vous</a>
+        </div>
+    </section>
+    
+</body>
+</html>
